4c49f7763e536437265079a1a6353680898982ac
lewuy0u <lewuyou@gmail.com>
2023-12-14 08:37:33 +0800

    翻译

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -242,21 +242,28 @@ class Dataset_Custom(Dataset):
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
 
+        # 如果预测对象为多变量预测或多元预测单变量
         if self.features == 'M' or self.features == 'MS':
+            # 取除日期外得所有列
             cols_data = df_raw.columns[1:]
             df_data = df_raw[cols_data]
+        # 若预测类型为S(单特征预测单特征)
         elif self.features == 'S':
+            # 取特征列
             df_data = df_raw[[self.target]]
 
+        # 数据归一化
         if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-
+        # 取日期列
         df_stamp = df_raw[['date']][border1:border2]
+        # 利用pandas将数据转换为日期格式
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        # 构建时间特征
         if self.timeenc == 0:
             df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
             df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
@@ -264,22 +271,30 @@ class Dataset_Custom(Dataset):
             df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
             data_stamp = df_stamp.drop(['date'], 1).values
         elif self.timeenc == 1:
+            # 时间特征构造函数，用来提取日期特征
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            # 转置
             data_stamp = data_stamp.transpose(1, 0)
-
+        # 取数据特征列
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
+        # 随机取得标签
         s_begin = index
+        # 训练区间
         s_end = s_begin + self.seq_len
+        # 有标签区间+无标签区间(预测时间步长)
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
 
+        # 取训练数据
         seq_x = self.data_x[s_begin:s_end]
         seq_y = self.data_y[r_begin:r_end]
+        # 取训练数据对应时间特征
         seq_x_mark = self.data_stamp[s_begin:s_end]
+        # 取有标签区间+无标签区间(预测时间步长)对应时间特征
         seq_y_mark = self.data_stamp[r_begin:r_end]
 
         return seq_x, seq_y, seq_x_mark, seq_y_mark
