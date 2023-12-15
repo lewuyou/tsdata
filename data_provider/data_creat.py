18f1ae7fc3247ba511228488b7e3ee09f876c9eb
lewuy0u <lewuyou@gmail.com>
2023-12-15 16:09:39 +0800

    33

--- data_provider/data_creat.py
+++ data_provider/data_creat.py
@@ -11,81 +11,6 @@ from pyti.relative_strength_index import  relative_strength_index as rsi
 from pyti.stochastic import percent_k as kdj_k
 from pyti.stochastic import percent_d as kdj_d
 
-def add_data(raw_data, args):
-    '''创建模型合适的数据集train_loader, valid_loader, test_loader'''
-
-    # 提取列数据为NumPy数组
-    open_data = raw_data['Open'].values
-    close_data = raw_data['Close'].values
-    high_data = raw_data['High'].values
-    low_data = raw_data['Low'].values
-    volume_data = raw_data['Volume'].values
-
-    # 指标计算
-    raw_data['macd'] = macd(close_data, 12, 26)
-    raw_data['rsi'] = rsi(close_data, 14)
-    raw_data['kdj_k'] = kdj_k(close_data, 9)
-    raw_data['kdj_d'] = kdj_d(close_data, 9)
-    raw_data['twf_feat'] = twf_feat(close_data, high_data, low_data, volume_data, 21)
-    raw_data['K_volume'] = volume_data / 1000  # 注意列名修正为 'K_volume'
-    print('\n指标计算完成正在添加指标到data....')
-    return raw_data
-
-def sub_data(raw_data,args):
-    '''
-    添加预测目标lable列,并将所有的nan值填充为0'''
-    lable_data = add_lable(raw_data,args.zhangfu,args.lable_n, args.lable_ch)
-    '''
-    删除第一列date列,删除前26行因为macd和cmf是空值,
-    删除最后config['feat_n']行因为未来lable的n天的数据是空值,影响训练'''
-    lable_data = lable_data.iloc[26:-args.lable_n, 1:]
-    '''
-    手动选择拼接前原始数据列'''
-    final_data = lable_data[args.final_data_feat].copy() 
-    print('拼接前data形状: ',final_data.shape)
-    print('拼接前数据有: ',args.final_data_feat)
-    '''
-    如果标签为True,那么就对final_data的change,twf列进行归一化'''
-    if args.feat_normalize == 1:
-        for i in args.feat_nor_lise:
-            final_data[i] = normalize(final_data[i])
-        print('拼接前对数据进行归一化处理')
-    elif args.feat_normalize == 2:
-        for i in args.feat_nor_lise:
-            final_data[i] = Standardization(final_data[i])
-        print('拼接前对数据进行标准化处理')
-    elif args.feat_normalize == 3:
-        for i in args.feat_nor_lise:
-            final_data[i] = QTF(final_data[i])
-        print('拼接前对数据进行均匀分布转换QuantileTransformer处理')
-    elif args.feat_normalize == 4:
-        for i in args.feat_nor_lise:
-            rg_data = final_data[i].values
-            Rank = RankGauss_norm()
-            final_data[i] = Rank.fit_transform(rg_data)
-        print('拼接前对数据进行高斯标准化Rank Gaussian Normalization处理')
-    elif args.feat_normalize == 5:
-        for i in args.feat_nor_lise:
-            final_data[i] = PowerTransformer(final_data[i])
-        print('拼接前对数据进行高斯分布转换PowerTransformer处理')
-    elif args.feat_normalize == 6:
-        for i in args.feat_nor_lise:
-            final_data[i] = RobustScaler(final_data[i])
-        print('拼接前对数据进行鲁棒缩放RobustScaler处理')
-
-def add_label(data, args):
-    '''原始数据计算并添加预测标签'''
-    data['Tom_Chg'] = (data['Close'].shift(-args.label_n) - data['Close'])/ data['Close']# 计算第n天到今天收益率
-    if args.label_ch:
-        # 初始化OT列为0
-        data['OT'] = 0
-        # 如果第n天到今天收益率data['Tom_Chg']大于0.5，那么label就等于1
-        data.loc[data['Tom_Chg'] >= args.zhangfu, 'OT'] = 1
-    else:
-        data['OT'] = data['Tom_Chg']
-    data = data.fillna(0)#将数据中的nan替换为0
-    return data
-
 def download_data(num,args):
     '''下载指定时间段数据'''
     if args.start_date == '0':
@@ -102,5 +27,52 @@ def download_data(num,args):
     df_resault = df_colum[['日期', '开盘', '收盘', '最高', '最低', '成交量', '振幅', '涨跌幅', '换手率']].copy()    # 只获取其中需要的数据
     df_resault.columns = ['date', 'Open', 'Close', 'High', 'Low', 'Volume', 'Amplitude', 'Change', 'Turnover']    # 重命名表头
     print(f'获取数据时间为： {args.num_start_data} - {args.num_end_data}')
-    print(f'数据形状： {df_resault.shape}')
+    print(f'原始数据形状： {df_resault.shape}')
     return df_resault
+
+
+def add_data(raw_data,args):
+    '''创建模型合适的数据集train_loader, valid_loader, test_loader'''
+
+    # 提取列数据为NumPy数组
+    open_data = raw_data['Open'].values
+    close_data = raw_data['Close'].values
+    high_data = raw_data['High'].values
+    low_data = raw_data['Low'].values
+    volume_data = raw_data['Volume'].values
+    
+    # 归一化 成交量 到 0-10 范围
+    min_volume = volume_data.min()
+    max_volume = volume_data.max()
+    raw_data['K_volume'] = 10 * (volume_data - min_volume) / (max_volume - min_volume)
+
+    # 指标计算
+    raw_data['macd'] = macd(close_data, 12, 26)
+    raw_data['rsi'] = rsi(close_data, 14)
+    raw_data['kdj_k'] = kdj_k(close_data, 9)
+    raw_data['kdj_d'] = kdj_d(close_data, 9)
+    raw_data['twf_feat'] = twf_feat(close_data, high_data, low_data, volume_data, 21)
+    print(f'添加数据以后形状： {raw_data.shape}')
+    return raw_data
+
+def add_label(data, args):
+    '''原始数据计算并添加预测标签'''
+    data['Tom_Chg'] = (data['Close'].shift(-args.label_n) - data['Close'])/ data['Close']# 计算第n天到今天收益率
+    if args.label_ch:
+        # 初始化OT列为0
+        data['OT'] = 0
+        # 如果第n天到今天收益率data['Tom_Chg']大于0.5，那么label就等于1
+        data.loc[data['Tom_Chg'] >= args.zhangfu, 'OT'] = 1
+    else:
+        data['OT'] = data['Tom_Chg']
+    data = data.fillna(0)#将数据中的nan替换为0
+    print(f'添加label以后数据形状: {data.shape}')
+    return data
+
+def sub_data(data,args):
+    # 删除前24行（macd算不出来）和最后的n行（最后n行未来涨跌幅是0）
+    label_data = data.iloc[25:-args.label_n, :]
+    # 指定删除不需要列
+    final_data = label_data.drop(columns=args.final_data_feat, errors='ignore')
+    print('删除指定行、列后数据形状: ',final_data.shape)
+    return final_data
